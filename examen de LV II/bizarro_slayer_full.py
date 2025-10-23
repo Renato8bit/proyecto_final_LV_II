b946ab1be72f45bfa859afa8cf57f985b36d48fd
Renato8bit <jvillagrapatino@gmail.com>
2025-10-23 18:38:32 -0300

    Menu
    principalmente creamos el menú para el juego(Nueva partida, ver historial y botón de salida)

--- examen de LV II/bizarro_slayer_full.py
+++ examen de LV II/bizarro_slayer_full.py
@@ -13,8 +13,15 @@ class MenuApp:
         self.root.geometry("500x450")
         ttk.Label(root, text= "Bizarro Slayer", font=("Helvetica",22)).pack(pady=20)
         ttk.Button(root, text= "Nueva Partida", command=self.nueva_partida).pack(pady=8)
-        ttk.Button(root, text= "Ver Historal/ Top", command1=self.ver_historial).pack(pady=8)
+        ttk.Button(root, text= "Ver Historal/ Top", command=self.ver_historial).pack(pady=8)
         ttk.Button(root, text= "Salir", command=self.root.destroy).pack(pady=8)
+
+    def nueva_partida(self):
+        self.root.destroy()
+        abrir_selector()
+
+    def ver_historial(self):
+        HistorialApp()
 
 if __name__ == "__main__" :
     root = tk.Tk()
